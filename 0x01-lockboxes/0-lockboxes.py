48a33901374f0c7cc03326de08c269823704b3d0
Chris-Effah <akboateng41@gmail.com>
2024-02-18 21:50:12 -0800

    fixed pycodestyle errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
+"""a program to unlock a number of boxes"""
+
+
 def canUnlockAll(boxes):
-    # Set to keep track of opened boxes
+    """Set to keep track of opened boxes"""
     opened_boxes = set()
 
-    # Initially, the first box is open
+    """Initially, the first box is open"""
     opened_boxes.add(0)
 
-    # List to keep track of keys obtained
+    """List to keep track of keys obtained"""
     keys_to_obtain = [0]
 
-    # Iterate through the keys and try to open boxes
+    """Iterate through the keys and try to open boxes"""
     while keys_to_obtain:
         current_key = keys_to_obtain.pop()
 
-        # Iterate through the keys found in the current box
+        """Iterate through the keys found in the current box"""
         for key in boxes[current_key]:
             # If the key opens a new box and it is not opened yet
             if key not in opened_boxes and key < len(boxes):
