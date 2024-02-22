04bac67f9881587f5ab8dd25f6bffcf14a7c7b45
akosuaeffa <akosuaeffah24@gmail.com>
2024-02-22 17:53:28 +0000

    changes

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    # Helper function to find prime factorization
+    def prime_factors(num):
+        factors = []
+        divisor = 2
+        while divisor * divisor <= num:
+            while (num % divisor) == 0:
+                factors.append(divisor)
+                num //= divisor
+            divisor += 1
+        if num > 1:
+            factors.append(num)
+        return factors
+
+    factors = prime_factors(n)
+    operations = 0
+
+    # Iterate through prime factors and sum them up
+    for factor in set(factors):
+        count = factors.count(factor)
+        operations += count
+
+    return operations
