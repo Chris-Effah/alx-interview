0c24adda5795b96ce75a5d5355c78665c9d50074
Chris-Effah <akboateng41@gmail.com>
2024-03-08 21:08:24 -0800

    bitwise

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def validUTF8(data):
+    """
+    a method that determines if a data set represents a valid UTF-8 encoding.
+    Return: True if data is a valid UTF-8 encoding, else return False
+    """
     num_bytes = 0
 
     for byte in data:
