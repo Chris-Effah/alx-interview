09ae63e6d39b3040bdc7052aa19c365dcd9a148e
Chris-Effah <akboateng41@gmail.com>
2024-03-17 11:00:21 -0700

    Committing local changes to 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,21 +7,51 @@ def validUTF8(data):
     a method that determines if a data set represents a valid UTF-8 encoding.
     Return: True if data is a valid UTF-8 encoding, else return False
     """
-    num_bytes = 0
+    if data == []:
+        return True
 
-    for byte in data:
-        if num_bytes == 0:
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7) != 0:
-                return True
+    binary_data = [bin(num)[2:].zfill(8) for num in data]
+
+    # Check for Overlong Encodings
+    if any(bin_num.startswith("0") for bin_num in binary_data):
+        return False
+
+    # Check for Invalid Continuation Bytes
+    if any(not bin_num.startswith("10") for bin_num in binary_data[1:]):
+        return False
+
+    # Validate Code Point Ranges
+    decoded_chars = [int(bin_num, 2) for bin_num in binary_data]
+    if any(0xD800 <= code_point <= 0xDFFF for code_point in decoded_chars):
+        return False
+
+    # Handle Overlong Sequences
+    decoded_str = "".join(chr(char) for char in decoded_chars)
+    reencoded_data = decoded_str.encode("utf-8")
+    if binary_data != [bin(byte)[2:].zfill(8) for byte in reencoded_data]:
+        return False
+
+    # Handle Maximum Code Point
+    if any(code_point > 0x10FFFF for code_point in decoded_chars):
+        return False
+
+    # Validate Sequence Lengths
+    expected_length = 0
+    for i, bin_num in enumerate(binary_data):
+        if i == 0:
+            if bin_num.startswith("110"):
+                expected_length = 2
+            elif bin_num.startswith("1110"):
+                expected_length = 3
+            elif bin_num.startswith("11110"):
+                expected_length = 4
+            else:
+                continue
+        elif bin_num.startswith("10"):
+            expected_length -= 1
         else:
-            if (byte >> 6) != 0b10:
-                return False
-            num_bytes -= 1
+            return False
+    if expected_length > 0:
+        return False
 
-    return num_bytes == 0
+    return True
